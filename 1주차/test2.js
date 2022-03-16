1f6a85797a5ec47c533e717a3ac8374e2275d8c4
sun2525 <ghd7673@naver.com>
2022-03-16 21:58:12 +0900

    3주차 과제 추가

--- 1주차/test2.js
+++ 1주차/test2.js
@@ -1,7 +1,7 @@
 document.write("<h1>if-else를 이용한 학점 매기기</h1>");
 document.write("<hr>");
 
-var num = prompt("이선홍 님 점수를 입력하세요");
+var num = prompt(" 님 점수를 입력하세요");
 if (num >= 100 && num <=90){
     c = 'A';
 }
